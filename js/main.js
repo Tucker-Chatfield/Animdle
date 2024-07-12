0c18276f87dc136a469091ff204e06c53a185dd8
tucker-chatfield <tucker.chatfield@gmail.com>
2024-07-12 12:22:23 -0500

    big changes to JS, finally got the keyboard to log letters in console.

--- js/main.js
+++ js/main.js
@@ -10,13 +10,17 @@ let nextSquare;
 let guessedAnimal;
 let guessedCount;
 /*------------------------ Cached Element References ------------------------*/
-// const messageEl = document.querySelector();
-// const resetBtn = document.getElementById();
-// const board = document.querySelectorAll();
+const messageEl = document.querySelector('.message');
+const resetBtn = document.getElementById('reset-btn');
+const board = document.querySelectorAll('#board > div');
+const letters = document.querySelectorAll('.key-row button');
 
 /*----------------------------- Event Listeners -----------------------------*/
+resetBtn.addEventListener('click', init);
 
 /*-------------------------------- Functions --------------------------------*/
+
+init();
 
 function init() {
   secretAnimal = animals[Math.floor(Math.random() * animals.length)].toUpperCase();
@@ -30,18 +34,38 @@ function init() {
   render();
   renderBoard();
 }
-
+  
 function render() {
-  renderMessage();
+  //renderMessage();
   renderKeyboard();
 }
-
+  
 function renderBoard() {
-  document.querySelectorAll('#board > div').forEach((div, i) => {
+  board.forEach((div) => {
     div.style.backgroundColor = '';
     div.innerText = '';
   })
-  
-
+    
+  letters.forEach((key) => {
+    key.style.backgroundColor = '';
+  })
+    
   messageEl.style.visibility = 'hidden';
+}
+  
+function renderKeyboard() { //logic in this function referenced: https://www.youtube.com/watch?v=j7OhcuZQ-q8
+  letters.forEach((letter) => {
+    letter.onclick = ({ target }) => {
+      const key = target.getAttribute('id');
+      console.log(key);  
+      // if(key === 'ENTER') {
+      //   handleSubmitGuess();
+      //   turn += 1;
+      //   return;
+      // } else if(key === 'DEL') {
+      //   handleDelete();
+      //   return;
+      // }
+    }
+  })
 }
